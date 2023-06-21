5d0e04881238199aeba2bcdc319d7d09baaf47de
ADHIRAJ PRATAP SINGH <adhiraj@plutos.one>
2023-06-21 12:35:07 +0530

    done

--- src/components/manage_campaign/modal.jsx
+++ src/components/manage_campaign/modal.jsx
@@ -14,23 +14,23 @@ import Slide from '@mui/material/Slide';
 import { Visibility } from '@mui/icons-material';
 import Alert from '@mui/material/Alert';
 // import Stack from '@mui/material/Stack';
-// import AddCircleIcon from '@mui/icons-material/AddCircle';
-// import DeleteIcon from "@mui/icons-material/Delete";
-// import Box from "@mui/material/Box";
-// import { DataGrid } from '@mui/x-data-grid';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import DeleteIcon from "@mui/icons-material/Delete";
+import Box from "@mui/material/Box";
+import { DataGrid } from '@mui/x-data-grid';
 
 // import Button from '@mui/material/Button';
-// import TextField from '@mui/material/TextField';
+import TextField from '@mui/material/TextField';
 // import Dialog from '@mui/material/Dialog';
-// import DialogActions from '@mui/material/DialogActions';
-// import DialogContent from '@mui/material/DialogContent';
-// import DialogContentText from '@mui/material/DialogContentText';
-// import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import QuickLinks from './quicklinks';
 import Partners from './partners';
 
 
-import { Container, Grid, Tooltip } from '@mui/material';
+import { Container, Grid , Tooltip } from '@mui/material';
 import Banner from './banner';
 
 
@@ -44,7 +44,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 // work from reducer - work pending - for testing purpose
 import {
-  getAllBannersOfCampaign, getQuickLinksOfBanner
+  getAllBannersOfCampaign
 } from "../../redux/actions/campaignmanager.actions"
 import DataTable from './quicklinks';
 
@@ -55,18 +55,15 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 
 export default function FullScreenDialog(props) {
 
-  const { campaignbanners, loading, quicklinks } = useSelector((state) => state.campaignBannerReducer);
+  const { campaignbanners, loading } = useSelector((state) => state.campaignBannerReducer);
   const [campdata, setCampData] = React.useState(props.data);
   const [open, setOpen] = React.useState(false);
-  const [openql, setOpenql] = React.useState(false);
-
   const dispatch = useDispatch();
 
   const handleClickOpen = (props) => {
     setOpen(true);
     // console.log(" bid = ",campdata.campaign_id );
     dispatch(getAllBannersOfCampaign(campdata.campaign_id));
-    dispatch(getQuickLinksOfBanner(campdata.campaign_id));
     // console.log(" dvl = ", campdata);
     // console.log(" camp banners : ",campaignbanners);
   };
@@ -107,9 +104,9 @@ export default function FullScreenDialog(props) {
                 Name of campaign - {campdata.campaign_name}
               </Typography>
             </Container>
-            {/* <Button autoFocus color="inherit" onClick={handleClose}>
+            <Button autoFocus color="inherit" onClick={handleClose}>
               save
-            </Button> */}
+            </Button>
           </Toolbar>
         </AppBar>
         <List>
@@ -147,11 +144,35 @@ export default function FullScreenDialog(props) {
           {/* <h1>QUICK LINKS </h1> */}
           <Grid container>
             <Grid lg={6} md={6} xs={12}>
-              <QuickLinks data={props}/>
+              <Typography variant="h5" align="center">
+                
+                  QUICKLINKS CENTER
+                  <Tooltip>
+                      <Button autoFocus onClick={handleClose} position="relative">
+                          <AddCircleIcon color="primary"/>
+                      </Button>
+
+                  </Tooltip>
+
+                
+              </Typography>
+              <DataTable />
             </Grid>
 
             <Grid lg={6} md={6} xs={12}>
-              <Partners data={props}/>
+            <Typography variant="h5" align="center">
+                
+                PARTNERS
+                <Tooltip>
+                    <Button autoFocus onClick={handleClose} position="relative">
+                        <AddCircleIcon color="primary"/>
+                    </Button>
+
+                </Tooltip>
+
+              
+            </Typography>
+              <DataTable />
             </Grid>
 
           </Grid>
